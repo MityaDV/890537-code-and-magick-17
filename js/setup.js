e087538f6d52c5586b106b3c32609ef3a55935b6
Dmitry Kovalkov <46884389+MityaDV@users.noreply.github.com>
2019-06-12 19:36:49 +0300

    Генерация 4 объктов магов, функция случайных значений

--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,15 @@
-/* eslint-disable no-redeclare */
+
 'use strict';
 
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон']; // массив имён
-var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг']; // массив фамилий
-var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)']; // массив цвета мантии
-var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green']; // массив цвета глаз
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_COUNT = 4;
+
+var randomValue = function (array) { // Функция генерации случайного значения из переданного массива
+  return array[Math.floor(Math.random() * array.length)];
+};
 
 var userDialog = document.querySelector('.setup'); // находим окно настроек пользователя
 userDialog.classList.remove('hidden'); // показываем окно настроек пользователя
@@ -15,28 +20,24 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template') /
 .content
 .querySelector('.setup-similar-item');
 
-// массив объектов с именами магов и цветами мантий и глаз
-var wizards = [];
+var wizards = []; // массив объектов с именами, цветами мантий, глаз магов
 
-// Функция генерации объектов
-
-var generateObjects = function (names, surname, coat, eyes) {
+var generateWizardObject = function () { // Функция генерации объектов
   return {
-    name: names[Math.floor(Math.random() * names.length)] + ' ' + surname[Math.floor(Math.random() * surname.length)],
-    coatColor: coat[Math.floor(Math.random() * coat.length)],
-    eyesColor: eyes[Math.floor(Math.random() * eyes.length)]
+    name: randomValue(WIZARD_NAMES) + ' ' + randomValue(WIZARD_SURNAMES),
+    coatColor: randomValue(WIZARD_COATS),
+    eyesColor: randomValue(WIZARD_EYES)
   };
 };
-// Добавляем четыре объекта
-for (var i = 0; i < 4; i++) {
-  wizards[i] = generateObjects(WIZARD_NAMES, WIZARD_SURNAMES, WIZARD_COATS, WIZARD_EYES);
+
+for (var i = 0; i < WIZARDS_COUNT; i++) { // Добавляем четыре объекта
+  wizards[i] = generateWizardObject();
 }
 
-// функция создания магов
-var renderWizard = function (wizard) {
+var renderWizard = function (wizard) { // функция создания магов
   var wizardElement = similarWizardTemplate.cloneNode(true); // записали в переменную клонирование шаблона
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name; // находим в клонах шаблона текст имен и заменяем их
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name; // находим в клонах шаблона текст имен и меняем их
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor; // меняем цвет плащей
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor; // меняем цвет глаз
 
@@ -45,8 +46,8 @@ var renderWizard = function (wizard) {
 
 var fragment = document.createDocumentFragment(); // создаём фрагмент
 
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i])); // добавляем в фрагмент на каждой итерации вызов ф-и для создания магов
+for (var j = 0; j < wizards.length; j++) {
+  fragment.appendChild(renderWizard(wizards[j])); // добавляем в фрагмент на каждой итерации вызов ф-и для создания магов
 }
 similarListElement.appendChild(fragment); // добавляем в блок для вставки подготовленный фрагмент
 

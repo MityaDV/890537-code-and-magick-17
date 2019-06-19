278a161595f904f1d2d1b1d05af11346618606eb
Dmitry Kovalkov <46884389+MityaDV@users.noreply.github.com>
2019-06-19 21:55:17 +0300

    код задания рабочий

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,7 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COATS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_FIREBALLS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var WIZARDS_COUNT = 4;
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
@@ -11,10 +12,34 @@ var ENTER_KEYCODE = 13;
 var setupOpen = document.querySelector('.setup-open'); // ноходим окно с аватаркой
 var userDialog = document.querySelector('.setup'); // находим окно настроек пользователя
 var setupClose = userDialog.querySelector('.setup-close'); // находим кнопку закрытия
+var setupUserName = userDialog.querySelector('.setup-user-name'); // находим поле ввода имени
 var similarListElement = document.querySelector('.setup-similar-list'); // находим блок куда будем копировать магов
 var similarWizardTemplate = document.querySelector('#similar-wizard-template') // находим шаблон который будем копировать
   .content
   .querySelector('.setup-similar-item');
+
+var setupWizardApperance = document.querySelector('.setup-wizard-appearance'); // блок настроек
+var setupWizardCoat = document.querySelector('.setup-wizard .wizard-coat'); // элемент плаща
+var setupWizardEyes = document.querySelector('.setup-wizard .wizard-eyes'); // элемент глаз
+var setupFireball = document.querySelector('.setup-fireball-wrap'); // блок фаерболла
+
+setupWizardCoat.addEventListener('click', function (evt) { // обработчик изменения цвета плаща
+  var coat = setupWizardApperance.querySelectorAll('[type="hidden"]')[0]; // нахожу скрытое поле
+  evt.target.style.fill = randomValueFromArray(WIZARD_COATS); // меняю цвет
+  coat.value = evt.target.style.fill; // заменяю данные для отправки на сервер
+});
+
+setupWizardEyes.addEventListener('click', function (evt) { // обработчик изменения цвета глаз
+  var eyes = setupWizardApperance.querySelectorAll('[type="hidden"]')[1];
+  evt.target.style.fill = randomValueFromArray(WIZARD_EYES);
+  eyes.value = evt.target.style.fill;
+});
+
+setupFireball.addEventListener('click', function (evt) { // обработчик изменения цвета фаербола
+  var fireball = setupFireball.querySelector('[type="hidden"]');
+  fireball.value = randomValueFromArray(WIZARD_FIREBALLS);
+  evt.currentTarget.style.background = fireball.value;
+});
 
 var onPopupEscPress = function (evt) { // вынесли и назвали обработчик события нажатия клавиши ESC для удобства его добавления и удаления
   if (evt.keyCode === ESC_KEYCODE) {
@@ -54,11 +79,19 @@ setupClose.addEventListener('keydown', function (evt) { // добавили об
   }
 });
 
+setupUserName.addEventListener('focus', function () { // удаляю обработчик esc при фокусе
+  document.removeEventListener('keydown', onPopupEscPress);
+});
+
+setupUserName.addEventListener('blur', function () { // возвращаю обработчик esc если фокус убран
+  document.addEventListener('keydown', onPopupEscPress);
+});
+
 var randomValueFromArray = function (array) { // Функция генерации случайного значения из переданного массива
   return array[Math.floor(Math.random() * array.length)];
 };
 
-var wizards = []; // массив объектов с именами, цветами мантий, глаз магов
+var wizards = []; // массив объектов с именами, цветами мантий и глаз магов
 
 var generateWizardObject = function () { // Функция генерации объектов
   return {
